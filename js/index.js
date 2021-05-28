aa11efe9750ab496c2be68341ef52055fb99113e
CharlotteEssajee <71342246+CharlotteEssajee@users.noreply.github.com>
2021-05-28 22:28:35 +0200

    updates for portfolio

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const url = "https://rainydays.flowerpower.one/wp-json/wc/store/products";
+const productContainer = document.querySelector(".products");
+
+async function getProducts() {
+  try {
+    const response = await fetch(url);
+    const getResults = await response.json();
+
+    const product = getResults;
+
+    createHTML(getResults);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getProducts();
+
+function createHTML(products) {
+  products.forEach(function (product) {
+    productContainer.innerHTML += `<a href="details.html?id=${product.id}" class="product">
+        <img src="${product.images[0].src}" alt="${product.name}">
+        <p class="productName">${product.name}</p>
+        <p class="productPrice">Price: ${product.prices.price}£</p>
+      </a>`;
+  });
+}
